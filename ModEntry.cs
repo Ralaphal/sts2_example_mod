61ca7905771d5f441afd88f841af5dcd059c584d
Olivier-tlse3 <131273830+Olivier-tlse3@users.noreply.github.com>
2026-03-30 07:24:49 +0200

    setup

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,12 +1,23 @@
-﻿using HarmonyLib;
-using MegaCrit.Sts2.Core.Modding;
+﻿using HarmonyLib;using Godot;
 
-[ModInitializer("Initialize")]
-public class ModEntry
+using MegaCrit.Sts2.Core.Modding;
+using MegaCrit.Sts2.Core.Logging;
+
+namespace FirstMod_Alpecor;
+
+[ModInitializer("ModLoaded")]
+public static class FirstMod_Alpecor
 {
-    public static void Initialize()
+    public static void ModLoaded()
     {
         var harmony = new Harmony("firstmod.patch");
         harmony.PatchAll();
+        Log.Warn("MOD FINISHED LOADING");
+
+        // Future: register your custom cards
+        // CardLibrary.Register(new MyCustomCard());
+
+        // Future: register your custom relics
+        // RelicLibrary.Register(new MyCustomRelic());
     }
 }
